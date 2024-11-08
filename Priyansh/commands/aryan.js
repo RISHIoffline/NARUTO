955db4e7035c3a75297da41a4ca9819dfbdbd7c1
RISHIoffline <rekhasolanki15081997@gmail.com>
2024-11-08 08:41:13 +0530

    Update aryan.js

--- Priyansh/commands/aryan.js
+++ Priyansh/commands/aryan.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "Aryan",
+	name: "DEVIL",
     version: "1.0.1",
 	hasPermssion: 2,
 	credits: "VanHung - Fixed by LTD", 
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("ARYAN")==0 || event.body.indexOf("Aryan")==0 || event.body.indexOf("Aaryan")==0 || event.body.indexOf("@Əɱɱ'ʌ Aʌʀƴʌŋ Ȏʬ'ɭɭɘx")==0) {
+	if (event.body.indexOf("DEVIL")==0 || event.body.indexOf("DEVIL")==0 || event.body.indexOf("DEVIL")==0 || event.body.indexOf("@Əɱɱ'ʌ ∂єνιℓ Ȏʬ'ɭɭɘx")==0) {
 		var msg = {
-				body: "हेलो बेबी मुझे आर्यन बाबू ने बनाया है। 🙂🌍🌸",
+				body: "हेलो बेबी मुझे मिस्टर डेविल ने बनाया है। 🙂🌍🌸",
 			}
 			api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("🙂", event.messageID, (err) => {}, true)
