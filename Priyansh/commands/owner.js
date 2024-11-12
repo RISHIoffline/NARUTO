35e5ceb8500a7b7e19c8259eef2834ec88fa50fd
RISHIoffline <rekhasolanki15081997@gmail.com>
2024-11-12 15:29:26 +0530

    Update owner.js

--- Priyansh/commands/owner.js
+++ Priyansh/commands/owner.js
@@ -20,23 +20,23 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = [
-"https://i.ibb.co/jgk6rB0/7ce2390636d1a46a331662dbaaf693d6.jpg"
+"https://i.ibb.co/f1ryvCt/027b05778314.png"
      ];
      var callback = () => api.sendMessage({body:`🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊🔰
 
-   𝐌𝐑..𝐀𝐀𝐑𝐘𝐀𝐍 𝐁𝐀𝐁𝐔•◡•)
+   𝐌𝐑..𝗗𝗘𝗩𝗜𝗟 𝗦𝗛𝗔𝗥𝗔𝗕𝗜 •◡•)
 
 𝐀𝐠𝐞 : 22
 
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 : 𝐒𝐈𝐍𝐆𝐋𝐄 𝐁𝐇𝐀𝐓𝐀𝐊 𝐑𝐇𝐀 𝐇𝐀𝐈
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 : 𝗭𝗔𝗜𝗡𝗔 𝗥𝗔𝗝𝗣𝗨𝗧
 
-𝐅𝐫𝐨𝐦 : 𝐆𝐎𝐑𝐀𝐊𝐇𝐏𝐔𝐑 (𝐔.𝐏)
+𝐅𝐫𝐨𝐦 : 𝗝𝗔𝗜𝗣𝗨𝗥 (𝗥𝗔𝗝𝗔𝗦𝗧𝗛𝗔𝗡)
 
 𝐒𝐭𝐮𝐝𝐲 : 𝐂𝐨𝐦𝐩𝐮𝐭𝐞𝐫 𝐏𝐫𝐨𝐠𝐫𝐚𝐦𝐦𝐢𝐧𝐠
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 https://www.facebook.com/MR.AARYAN.HERE:
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 https://www.facebook.com/profile.php?id=61560621821421/MR.DEVIL.HERE:
 
-𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 : +919664945791
+𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 : +919024870456
 
 Jai Shree RaaM 🚩🌍❤️🙂!❤🙂♣️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
