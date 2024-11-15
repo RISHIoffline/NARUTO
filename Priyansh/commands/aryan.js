be565c68b40c586fb99109d1b22cfd42ca95280a
RISHIoffline <rekhasolanki15081997@gmail.com>
2024-11-15 18:14:51 +0530

    Update aryan.js

--- Priyansh/commands/aryan.js
+++ Priyansh/commands/aryan.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "Aryan",
+	name: "DEVIL",
     version: "1.0.1",
 	hasPermssion: 2,
 	credits: "VanHung - Fixed by LTD", 
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("ARYAN")==0 || event.body.indexOf("Aryan")==0 || event.body.indexOf("Aaryan")==0 || event.body.indexOf("@Əɱɱ'ʌ Aʌʀƴʌŋ Ȏʬ'ɭɭɘx")==0) {
+	if (event.body.indexOf("DEVIL")==0 || event.body.indexOf("DEVIL")==0 || event.body.indexOf("DEVIL")==0 || event.body.indexOf("@Əɱɱ'ʌ DEVIL Ȏʬ'ɭɭɘx")==0) {
 		var msg = {
 				body: "हेलो बेबी मुझे डेविल शराबी बनाया है। 🙂🌍🌸",
 			}
